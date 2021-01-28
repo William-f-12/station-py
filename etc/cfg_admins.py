33d03f0eb24acc162bd0407641aa30ff9db1d39f
William-f-12 <3326007521@qq.com>
2021-01-28 11:49:16 +0800

    注销baloo管理员账号

--- etc/cfg_admins.py
+++ etc/cfg_admins.py
@@ -9,7 +9,7 @@
 
 administrators = [
     # 'moky@4DnqXWdTV8wuZgfqSCX9GjE2kNq7HJrUgQ',
-    'baloo@4LA5FNbpxP38UresZVpfWroC2GVomDDZ7q',
+    # 'baloo@4LA5FNbpxP38UresZVpfWroC2GVomDDZ7q',
 
     # 'pigboy@4cikGaYjWGam955VmYbH3rAq1oN7MGcD7K',
 ]
